24ad738380b2176a1850621f6764c566dbc94fc7
suibfh <wcoin.game@gmail.com>
2026-06-17 10:46:18 +0900

    Delete mch-chain-battle directory

--- mch-chain-battle/src/effects.js
+++
@@ -1,6 +0,0 @@
-export function flashClass(element, className) {
-  if (!element) return;
-  element.classList.remove(className);
-  void element.offsetWidth;
-  element.classList.add(className);
-}
